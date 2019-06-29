71c523435d4428b7b09b95dba1db9c28f336e494
Sergio Souza Novak <serginhosnovak@hotmail.com>
2019-06-29 10:57:41 -0300

    feito deletar de veiculo

--- src/app/deletar-veiculo/deletar-veiculo.component.ts
+++ src/app/deletar-veiculo/deletar-veiculo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import Veiculo from '../../../model/Veiculo';
+import { DeletarVeiculoService } from './deletar-veiculo.service';
+import { EditarVeiculoService } from '../editar-veiculo/editar-veiculo.service';
+
 
 @Component({
   selector: 'app-deletar-veiculo',
@@ -6,10 +10,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deletar-veiculo.component.scss']
 })
 export class DeletarVeiculoComponent implements OnInit {
-
-  constructor() { }
+  editadoComSucesso:boolean = false;
+  numeroSerieBusca:any;
+  veiculo:Veiculo;  
+  CPFexiste:boolean = true;
+  podeEditar = false;
+  
+  
+  constructor(private editarVeiculoService:EditarVeiculoService,private deletarVeiculoService:DeletarVeiculoService) { 
+    this.veiculo = new Veiculo("","","Manual",0,"","");
+  }
 
   ngOnInit() {
   }
+
+  buscar(){
+    this.editarVeiculoService.getVeiculo(this.numeroSerieBusca).subscribe(response => {
+      
+      if(response.result){
+        this.podeEditar=true;
+        this.veiculo.modelo = response.result.modelo ; 
+        this.veiculo.montadora = response.result.montadora ; 
+        this.veiculo.cambio = response.result.cambio ; 
+        this.veiculo.numeroSerie = response.result.numeroSerie ; 
+        this.veiculo.proprietario = response.result.proprietario ; 
+        this.veiculo.ano = response.result.ano;
+        this.CPFexiste = true;
+        this.podeEditar=true;
+
+
+      }
+     else if(response.result==null){
+        this.CPFexiste = false;
+        this.zerar();
+        this.podeEditar=false
+        this.editadoComSucesso= false;
+      }
+     
+      console.log(response)
+      })
+  }
+
+  zerar(){
+      this.veiculo.zerar();
+  }
+
+  salvar(){
+      this.deletarVeiculoService.delete(this.veiculo).subscribe(response => {
+        this.editadoComSucesso=true;
+      this.veiculo.zerar();
+        
+      })
+      
+
+
+
+  }  
 
 }
--- src/app/deletar-veiculo/deletar-veiculo.service.ts
+++ src/app/deletar-veiculo/deletar-veiculo.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
-
+import { api } from '../../environments/environment';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import Veiculo from '../../../model/Veiculo';
 @Injectable({
   providedIn: 'root'
 })
 export class DeletarVeiculoService {
 
-  constructor() { }
-}
+  constructor(private http:HttpClient) { }
+
+
+  delete(veiculo:Veiculo){
+
+
+    let params = new HttpParams()
+    .set("modelo",""+veiculo.modelo)
+    .set("montadora",""+veiculo.montadora)
+    .set("cambio",""+veiculo.cambio)
+    .set("numeroSerie",""+veiculo.numeroSerie)
+    .set("proprietario",""+veiculo.proprietario)
+    .set("ano",""+veiculo.ano);
+console.log(veiculo)
+     return this.http.post<any>(api.url+'api/deletar-veiculo/',{veiculo:veiculo},{ headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+    })
+
+    
+
+
+
+
+  }}
