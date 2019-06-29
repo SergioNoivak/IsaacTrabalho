3c3e953e0d6e27daa716f05a0603d85d887f2da3
Sergio Souza Novak <serginhosnovak@hotmail.com>
2019-06-29 10:25:20 -0300

    feito editar de veiculo

--- src/app/editar-veiculo/editar-veiculo.component.ts
+++ src/app/editar-veiculo/editar-veiculo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import Veiculo from '../../../model/Veiculo';
+import { EditarVeiculoService } from './editar-veiculo.service';
 @Component({
   selector: 'app-editar-veiculo',
   templateUrl: './editar-veiculo.component.html',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarVeiculoComponent implements OnInit {
 
-  constructor() { }
+  editadoComSucesso:boolean = false;
+  numeroSerieBusca:any;
+  veiculo:Veiculo;  
+  CPFexiste:boolean = true;
+  podeEditar = false;
+  
+  
+  constructor(private editarVeiculoService:EditarVeiculoService) { 
+    this.veiculo = new Veiculo("","","Manual",0,"","2000");
+  }
 
   ngOnInit() {
   }
 
+  buscar(){
+    this.editarVeiculoService.getVeiculo(this.numeroSerieBusca).subscribe(response => {
+      
+      if(response.result){
+        this.podeEditar=true;
+        this.veiculo.modelo = response.result.modelo ; 
+        this.veiculo.montadora = response.result.montadora ; 
+        this.veiculo.cambio = response.result.cambio ; 
+        this.veiculo.numeroSerie = response.result.numeroSerie ; 
+        this.veiculo.proprietario = response.result.proprietario ; 
+        this.veiculo.ano = response.result.ano;
+        this.CPFexiste = true;
+        this.podeEditar=true;
+
+
+      }
+     else if(response.result==null){
+        this.CPFexiste = false;
+        this.zerar();
+        this.podeEditar=false
+        this.editadoComSucesso= false;
+      }
+     
+      console.log(response)
+      })
+  }
+
+  zerar(){
+      this.veiculo.zerar();
+  }
+
+  salvar(){
+    console.log("dsadas") ;
+      this.editarVeiculoService.setVeiculo(this.veiculo,this.numeroSerieBusca).subscribe(response => {
+        this.editadoComSucesso=true;
+      this.veiculo.zerar();
+        
+      })
+      
+
+
+
+  }
 }
--- src/app/editar-veiculo/editar-veiculo.service.ts
+++ src/app/editar-veiculo/editar-veiculo.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
-
+import Veiculo from '../../../model/Veiculo';
+import { api } from '../../environments/environment';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class EditarVeiculoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public  getVeiculo(numeroSerie:string){
+
+    console.log(`${api.url}`)
+    let that = this;
+    let params = new HttpParams().set("numeroSerie",numeroSerie) //Create new HttpParams
+     return this.http.get<any>(api.url+'api/buscar-veiculo/',{ headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+     , params:params}).pipe(
+          );
+
+
+
+  }
+
+
+  
+  setVeiculo(veiculo:Veiculo,numeroSerie:any){
+
+    let params = new HttpParams()
+    .set("modelo",""+veiculo.modelo)
+    .set("montadora",""+veiculo.montadora)   
+    .set("cambio",""+veiculo.cambio)
+    .set("numeroSerie",""+veiculo.numeroSerie)   
+    .set("proprietario",""+veiculo.proprietario)
+    .set("ano",""+veiculo.ano);
+console.log(veiculo)
+     return this.http.post<any>(api.url+'api/alterar-veiculo/',{veiculo:veiculo,oldNumeroSerie:numeroSerie},{ headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+    })
+
+    
+  }
+
 }
