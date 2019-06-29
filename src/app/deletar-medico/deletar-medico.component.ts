43213b6ed28de8c8db3785a537304f070fd19969
Sergio Souza Novak <serginhosnovak@hotmail.com>
2019-06-28 23:05:40 -0300

    implementado CRUD de medico sem validação na parte do cliente

--- src/app/deletar-medico/deletar-medico.component.ts
+++ src/app/deletar-medico/deletar-medico.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Medico from 'model/Medico';
+import { EditarMedicoService } from '../editar-medico/editar-medico.service';
+import { DeletarMedicoService } from './deletar-medico.service';
 
 @Component({
   selector: 'app-deletar-medico',
@@ -6,10 +9,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deletar-medico.component.scss']
 })
 export class DeletarMedicoComponent implements OnInit {
-
-  constructor() { }
+editadoComSucesso:boolean = false;
+  CPFbusca:any;
+  medico:Medico;  
+  CPFexiste:boolean = true;
+  podeEditar = false;
+  
+  
+  constructor(private editarMedicoService:EditarMedicoService,private deletarMedicoService:DeletarMedicoService) { 
+    this.medico = new Medico("","","Vespertino",0,"","");
+  }
 
   ngOnInit() {
   }
+
+  buscar(){
+    this.editarMedicoService.getMedico(this.CPFbusca).subscribe(response => {
+      
+      if(response.result){
+        this.podeEditar=true;
+        this.medico.nome = response.result.nome ; 
+        this.medico.CRM = response.result.CRM ; 
+        this.medico.CPF = response.result.CPF ; 
+        this.medico.predio = response.result.predio ; 
+        this.medico.setor = response.result.setor ; 
+        this.medico.periodoAtendimento = response.result.periodoAtendimento;
+        this.CPFexiste = true;
+        this.podeEditar=true;
+
+
+      }
+     else if(response.result==null){
+        this.CPFexiste = false;
+        this.zerar();
+        this.podeEditar=false
+        this.editadoComSucesso= false;
+      }
+     
+      console.log(response)
+      })
+  }
+
+  zerar(){
+      this.medico.zerar();
+  }
+
+  salvar(){
+      this.deletarMedicoService.delete(this.medico).subscribe(response => {
+        this.editadoComSucesso=true;
+      this.medico.zerar();
+        
+      })
+      
+
+
+
+  }  
 
 }
--- src/app/deletar-medico/deletar-medico.service.ts
+++ src/app/deletar-medico/deletar-medico.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
-
+import Medico from '../../../model/Medico';
+import { api } from '../../environments/environment';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class DeletarMedicoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  delete(medico:Medico){
+
+
+    let params = new HttpParams()
+    .set("CPF",""+medico.CPF)
+    .set("CRM",""+medico.CRM)
+    .set("nome",""+medico.nome)
+    .set("periodoAtendimento",""+medico.periodoAtendimento)
+    .set("predio",""+medico.predio)
+    .set("setor",""+medico.setor);
+console.log(medico)
+     return this.http.post<any>(api.url+'api/deletar-medico/',{medico:medico},{ headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+    })
+
+    
+
+
+
+
+  }
 }
